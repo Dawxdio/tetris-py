6ff11bdcf49c7fcb26a5cc7ad70be7e53562ace1
Dawxdio <119059709+Dawxdio@users.noreply.github.com>
2024-11-27 23:18:21 +0100

    movement :D

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from os import name, system
 from time import sleep
 from msvcrt import getch
+from random import choice
 
 
 def generate():
+    return choice(list(pieces.keys()))
+
+
+def refresh(cords):
+    for k in cords:
+        state[k[0]][k[1]] = "@"
     return
 
 
@@ -10,80 +18,81 @@ def rotate():
     return
 
 
-def move():
-    return
+def move(val, cords, st):
+    for k in cords:
+        st[k[0]][k[1]] = " "
+    return [[k[0], k[1]+val] for k in cords], st
 
 
-def drop():
-    return
-
-
-def keep():
-    return
+def drop(cords, st):
+    for k in cords:
+        st[k[0]][k[1]] = " "
+    return [[k[0]+1, k[1]] for k in cords], st
 
 
 def reset():
     return
 
 
-state = [[" " for _ in range(10)] for _ in range(20)]
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
+
+pieces = {
+    "Z": [[0, 3], [0, 4], [1, 4], [1, 5]],
+    "Z_reverse": [[0, 4], [0, 5], [1, 3], [1, 4]],
+    "L": [[0, 5], [1, 3], [1, 4], [1, 5]],
+    "L_reverse": [[0, 3], [1, 3], [1, 4], [1, 5]],
+    "Square": [[0, 4], [0, 5], [1, 4], [1, 5]],
+    "T": [[0, 5], [1, 4], [1, 5], [1, 6]],
+    "|": [[1, 3], [1, 4], [1, 5], [1, 6]]
+}
+# TODO: change coordinate order (y,x) => (x,y)
+state = [[" " for _ in range(10)] for _ in range(21)]
+
+current_piece = generate()
+piece_coordinates = pieces[current_piece]
+store = []
 while True:
-    print(" - - - - - - - - - - ")
-    for i in range(20):
-        print("|", end="")
-        for j in range(10):
-            print(state[i][j], end=" ")
-        print("|")
-    print(" - - - - - - - - - - ")
-    keycode = ord(getch())
+    keycode = ord(getch())  # fetch keyboard input
     if keycode == 72:  # Up arrow
         print("rotate right")
     elif keycode == 75:  # Left arrow
-        print("move left")
+        # check if x coordinate after move would be out of range
+        if 0 not in [i[1] for i in piece_coordinates]:
+            piece_coordinates, state = move(-1, piece_coordinates, state)
     elif keycode == 77:  # Right arrow
-        print("move right")
+        # check if x coordinate after move would be out of range
+        if 9 not in [i[1] for i in piece_coordinates]:
+            piece_coordinates, state = move(1, piece_coordinates, state)
     elif keycode == 80:  # Down arrow
-        print("slow drop")
+        try:
+            piece_coordinates, state = drop(piece_coordinates, state)
+        except IndexError:
+            continue
     elif keycode == 99:  # "c"
-        print("keep")
+        temp = current_piece
+        if store:
+            current_piece = store
+            refresh(pieces[store])
+        else:
+            current_piece = generate()
+        store = temp
+
     elif keycode == 114:  # "r"
         print("reset")
     elif keycode == 32:  # Space
         print("instant drop")
-#
-# def clear():
-#     if name == 'nt':
-#         _ = system('cls')
-#     else:
-#         _ = system('clear')
 
-#
-# while True:
-#     print("""
-#     | - - - - - - - - - - |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     |                     |
-#     | _ _ _ _ _ _ _ _ _ _ |
-#     """)
-#     sleep(1)
-#     a=input()
-#     clear()
+    refresh(piece_coordinates)
+    print("-----------------------")
+    for i in range(1, 21):
+        print("||", end="")
+        for j in range(9):
+            print(state[i][j], end=",")
+        print(state[i][9], end="")
+        print("||")
+    print("-----------------------")
